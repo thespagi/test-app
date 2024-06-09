1b0df2661a351499db1716df7fa6139e8f4c77fb
TheSpagi <thespagi@icloud.com>
2024-06-09 04:06:38 +0200

    chore: Create basic rest api

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,9 @@
-console.log("Hello, world!");
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  await app.listen(3000);
+}
+
+bootstrap();
